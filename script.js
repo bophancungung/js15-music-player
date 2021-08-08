f2037dabdc565fd967cb4c069426d1ea2b03e135
bophancungung <bophancungung@gmail.com>
2021-08-08 11:53:30 +0700

    music player with js

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+let songIndex = 0;
+
+// Mucsic
+const songs = [
+  {
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
+];
 
 // Chech if playing
 let isPlaying = false;
@@ -22,5 +50,30 @@ function pauseSong() {
   isPlaying = false;
 }
 
-// Play or pause event listener
+// Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+function nextSong() {
+  songIndex < songs.length-1 ? songIndex++ : songIndex = 0;
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function prevSong() {
+  songIndex > 0 ? songIndex-- : songIndex = songs.length-1;
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Event listener
 playBtn.addEventListener('click', () => isPlaying ? pauseSong() : playSong());
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// On load - Select first song
+loadSong(songs[songIndex]);
